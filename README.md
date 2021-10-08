f4dfa4288e6dc3b7ebb5cc6eb7662dfc888c9f0b
jovanhuang <jhtc98@hotmail.com>
2021-10-08 23:30:55 +0800

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # E-commerce-Website
+
 This is done using Flask Python web framework.
 There is an authentication system for this E-commerce Website.
+
+1. Create a virtual env and do ".\env\Scripts\activate" to activate it
---
+++ market/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+#This secret key is generated once by doing os.urandom(12).hex() in cmdline. This is a security layer needed to make forms work.
+app.config['SECRET_KEY'] = 'a6281b9267cd2386fa4f8474'
+db = SQLAlchemy(app)
+
+# to let __init__ file recognize the routes we created -- when import, then python will execute routes.py
+from market import routes
+
+
